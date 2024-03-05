8f7b73c20a8a2d98278a356dbc0c6ab83d112859
Bharti Kumari <bhartik021@gmail.com>
2024-03-05 22:57:17 +0530

    index.js updated

--- src/constants/index.js
+++ src/constants/index.js
@@ -66,10 +66,6 @@ import {
     },
   ];
   
-  export const heroIcons = [homeSmile, file02, searchMd, plusSquare];
-  
-  export const notificationImages = [notification4, notification3, notification2];
-  
   export const companyLogos = [yourlogo, yourlogo, yourlogo, yourlogo, yourlogo];
   
   export const brainwaveServices = [
