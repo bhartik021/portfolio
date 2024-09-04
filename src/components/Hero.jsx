98d9f27877c9fd336f741874574411febcd052a9
bhartik021 <bhartik021@gmail.com>
2024-09-04 07:32:16 +0530

    updated bio

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -33,7 +33,7 @@ const Hero = () => {
             </h1>
             <p className="body-1 max-w-3xl mx-auto mb-6
             text-n-2 lg:mb-8">
-            Amazon ML Summer'23 | SIH'23 Finalist | Samsung SFT - Top 50 | Mentor at GSSoC'23 | Technical Writer
+            Software Engineer @miniOrange | Amazon ML Summer'23 | SIH'23 Finalist | Samsung SFT - Top 50 | Mentor at GSSoC'23 | Technical Writer | Gold mlsa | Google Women TechMaker
             </p>
             <Button target="_blank" href="https://calendly.com/bhartik021" white>
                 Book a Meeting
